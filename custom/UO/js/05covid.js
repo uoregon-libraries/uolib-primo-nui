b528da3c2eb48a8050168e8be997789b90abacaf
Jeremy Echols <jechols@uoregon.edu>
2020-08-04 08:19:35 -0700

    Reduce size of covid banner

--- custom/UO/js/05covid.js
+++ custom/UO/js/05covid.js
@@ -1,10 +1,12 @@
 //Add Emergency Banner
 app.component('prmBackToLibrarySearchButtonAfter', {
   template: `
-    <div id="covid-19">
-      <h2>COVID-19 Notice</h2>
-      <span>UO Libraries buildings are closed, but we're here to help!</span>
-      <a href="https://library.uoregon.edu/get-help">Get help from UO Libraries</a>
+    <div class="covid-wrapper">
+      <div id="covid-19">
+        <h2>COVID-19 Notice</h2>
+        <span>UO Libraries buildings are closed, but we're here to help!</span>
+        <a href="https://library.uoregon.edu/get-help">Get help from UO Libraries</a>
+      </div>
     </div>
   `,
   scope: {},
