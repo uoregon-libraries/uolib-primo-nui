ae9358c9e987ee20f523ba1466deb5c374124edb
Jeremy Echols <jechols@uoregon.edu>
2020-08-04 09:19:24 -0700

    Use better region for covid notice

--- custom/UO/js/05covid.js
+++ custom/UO/js/05covid.js
@@ -1,13 +1,13 @@
 //Add Emergency Banner
 app.component('prmBackToLibrarySearchButtonAfter', {
   template: `
-    <div class="covid-wrapper">
+    <aside class="covid-wrapper">
       <div id="covid-19">
         <h2>COVID-19 Notice</h2>
         <span>UO Libraries buildings are closed, but we're here to help!</span>
         <a href="https://library.uoregon.edu/get-help">Get help from UO Libraries</a>
       </div>
-    </div>
+    </aside>
   `,
   scope: {},
   bindings: {parentCtrl: '<'},
