445855f2c18499b4c5e618d0968b6c6b070f67b0
Jeremy Echols <jechols@uoregon.edu>
2020-08-04 07:48:26 -0700

    Update covid notice

--- custom/UO/js/05covid.js
+++ custom/UO/js/05covid.js
@@ -3,9 +3,8 @@ app.component('prmBackToLibrarySearchButtonAfter', {
   template: `
     <div id="covid-19">
       <h2>COVID-19 Notice</h2>
-      Due to the impacts of COVID-19, Summit requesting for physical materials
-      is no longer available until further notice. Sign in to see more options or
-      <a href="https://library.uoregon.edu/ask">Ask a Librarian</a> for help.
+      <span>UO Libraries buildings are closed, but we're here to help!</span>
+      <a href="https://library.uoregon.edu/get-help">Get help from UO Libraries</a>
     </div>
   `,
   scope: {},
