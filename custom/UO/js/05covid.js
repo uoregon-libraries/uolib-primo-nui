59097dd122101bdb1ba3bedb688669fd7ee2d8b3
Jeremy Echols <jechols@uoregon.edu>
2020-12-16 08:25:49 -0800

    Commit prior changes

--- custom/UO/js/05covid.js
+++ custom/UO/js/05covid.js
@@ -4,9 +4,10 @@ app.component('prmBackToLibrarySearchButtonAfter', {
     <aside class="site-alert">
       <div class="info">
         <div class="hours-notice">
-          UO Libraries building hours and services vary - please check the
-          <a href="https://library.uoregon.edu/">Libraries homepage</a> for today's hours. For more information,
-          <a href="https://library.uoregon.edu/get-help">get help from UO Libraries</a>.
+          UO Libraries building hours and services vary - please view the
+          <a href="https://library.uoregon.edu/hours-and-locations">Hours &amp; Locations</a>
+          for today's hours. For more information,
+          <a href="https://library.uoregon.edu/get-help">get help from UO Libraries.</a>
         </div>
       </div>
     </aside>
