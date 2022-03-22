9c3cd84e770ff2da418f41849b393c70110c73d1
Jeremy Echols <jechols@uoregon.edu>
2022-03-22 03:56:34 -0700

    Update to the latest browzine integration
    Updates us to the code that allows use of unpaywall features

--- custom/01ALLIANCE_UO-UO/js/30-browzine.js
+++ custom/01ALLIANCE_UO-UO/js/30-browzine.js
@@ -1,13 +1,11 @@
 /* Browzine link out - shows up as "View Issue Contents" or "View Journal
  * Contents" in the same area where HathiTrust link is displayed (generally
- * near the availability info, like "Available at Mathematics Library..." */
-app.component('prmSearchResultThumbnailContainerAfter', {
-  bindings: { parentCtrl: '<' },
-  template: '<browzine-thumbnail parent-ctrl="$ctrl.parentCtrl"></browzine-thumbnail>'
-});
-app.controller('prmSearchResultAvailabilityLineAfterController', function($scope) {
-  window.browzine.primo.searchResult($scope);
-});
+ * near the availability info, like "Available at Mathematics Library..."
+ *
+ * See https://thirdiron.atlassian.net/wiki/spaces/BrowZineAPIDocs/pages/79200260/Ex+Libris+Primo+Integration
+ * for the latest Third Iron integration details. Currently using the
+ * information they provided as of March 21, 2022.
+ */
 
 // Begin BrowZine - Primo Integration...
   window.browzine = {
@@ -19,16 +17,48 @@ app.controller('prmSearchResultAvailabilityLineAfterController', function($scope
     journalBrowZineWebLinkTextEnabled: true,
     journalBrowZineWebLinkText: "View Journal Contents",
 
-    acticleBrowZineWebLinkTextEnabled: true,
+    articleBrowZineWebLinkTextEnabled: true,
     articleBrowZineWebLinkText: "View Issue Contents",
 
     articlePDFDownloadLinkEnabled: true,
     articlePDFDownloadLinkText: "Download PDF",
 
+    articleLinkEnabled: true,
+    articleLinkText: "Read Article",
+
     printRecordsIntegrationEnabled: true,
+
+    showFormatChoice: false,
+    showLinkResolverLink: true,
+
+    articleRetractionWatchEnabled: true,
+    articleRetractionWatchText: "Retracted Article",
+
+    unpaywallEmailAddressKey: "libsys-isd@ithelp.uoregon.edu",
+
+    articlePDFDownloadViaUnpaywallEnabled: true,
+    articlePDFDownloadViaUnpaywallText: "Download PDF (via Unpaywall)",
+
+    articleLinkViaUnpaywallEnabled: true,
+    articleLinkViaUnpaywallText: "Read Article (via Unpaywall)",
+
+    articleAcceptedManuscriptPDFViaUnpaywallEnabled: true,
+    articleAcceptedManuscriptPDFViaUnpaywallText: "Download PDF (Accepted Manuscript via Unpaywall)",
+
+    articleAcceptedManuscriptArticleLinkViaUnpaywallEnabled: true,
+    articleAcceptedManuscriptArticleLinkViaUnpaywallText: "Read Article (Accepted Manuscript via Unpaywall)",
   };
 
   browzine.script = document.createElement("script");
   browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
   document.head.appendChild(browzine.script);
+
+  app.controller('prmSearchResultAvailabilityLineAfterController', function($scope) {
+    window.browzine.primo.searchResult($scope);
+  });
+
+  app.component('prmSearchResultAvailabilityLineAfter', {
+    bindings: { parentCtrl: '<' },
+    controller: 'prmSearchResultAvailabilityLineAfterController'
+  });
 // ... End BrowZine - Primo Integration
