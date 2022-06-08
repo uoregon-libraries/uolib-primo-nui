c6d89ffa364b3cb57a4fd23f69eafe794e469dba
Jeremy Echols <jechols@uoregon.edu>
2022-06-08 12:34:02 -0700

    Add banner to let people know about VE changes

---
+++ custom/01ALLIANCE_UO-UO/js/99-alert-ve-info.js
@@ -0,0 +1,18 @@
+// Banner for general alerts - copy this to custom/UO/js/99alert.js and add
+// whatever custom alert messaging is necessary. Delete that file when the
+// alert is no longer relevant.
+app.component('prmBackToLibrarySearchButtonAfter', {
+  template: `
+    <aside class="site-alert">
+      <div class="info">
+        <div class="alert-notice">
+          LibrarySearch was upgraded on June 28th, 2022. See the
+          <a href="https://researchguides.uoregon.edu/librarysearch-upgrade">
+          potential changes and impacts to your data</a>.
+        </div>
+      </div>
+    </aside>
+  `,
+  scope: {},
+  bindings: {parentCtrl: '<'},
+});
