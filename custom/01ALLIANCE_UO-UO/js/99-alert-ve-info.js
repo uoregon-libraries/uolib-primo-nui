2ec2d538b1a6c6cdf7a8112ec17c110433e1c419
Jeremy Echols <jechols@uoregon.edu>
2022-06-28 08:09:32 -0700

    Change upgrade banner wording

--- custom/01ALLIANCE_UO-UO/js/99-alert-ve-info.js
+++ custom/01ALLIANCE_UO-UO/js/99-alert-ve-info.js
@@ -6,9 +6,10 @@ app.component('prmBackToLibrarySearchButtonAfter', {
     <aside class="site-alert">
       <div class="info">
         <div class="alert-notice">
-          LibrarySearch was upgraded on June 28th, 2022. See the
-          <a href="https://researchguides.uoregon.edu/librarysearch-upgrade">
-          potential changes and impacts to your data</a>.
+          LibrarySearch was upgraded on June 28th, 2022. Please update your bookmarks for LibrarySearch to
+          <a href="https://alliance-uoregon.primo.exlibrisgroup.com/discovery/search?vid=01ALLIANCE_UO:UO">https://alliance-uoregon.primo.exlibrisgroup.com/discovery/search?vid=01ALLIANCE_UO:UO</a>
+          and update any saved permalinks.
+          <a href="https://researchguides.uoregon.edu/librarysearch-upgrade">More information about changes.</a>
         </div>
       </div>
     </aside>
