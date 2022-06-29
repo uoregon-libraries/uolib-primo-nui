f6b2e120d742c6e289e714c0d91ae322686332bd
Jeremy Echols <jechols@uoregon.edu>
2022-06-29 07:41:04 -0700

    Ensure alert shows on full record views

--- custom/UO/js/99alert-ve-info.js
+++ custom/UO/js/99alert-ve-info.js
@@ -18,3 +18,21 @@ app.component('prmBackToLibrarySearchButtonAfter', {
   scope: {},
   bindings: {parentCtrl: '<'},
 });
+
+app.component('prmBackToSearchResultsButtonAfter', {
+  template: `
+    <aside class="site-alert">
+      <div class="info">
+        <div class="alert-notice">
+          LibrarySearch has moved to a new location. In order to get complete
+          results and to request items, please go to
+          <a href="https://alliance-uoregon.primo.exlibrisgroup.com/discovery/search?vid=01ALLIANCE_UO:UO">https://alliance-uoregon.primo.exlibrisgroup.com/discovery/search?vid=01ALLIANCE_UO:UO</a>.
+
+          <a href="https://researchguides.uoregon.edu/librarysearch-upgrade">More information about changes.</a>
+        </div>
+      </div>
+    </aside>
+  `,
+  scope: {},
+  bindings: {parentCtrl: '<'},
+});
