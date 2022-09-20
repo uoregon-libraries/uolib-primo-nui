6d4959cbd29d8a530c5ce3a26d5251d05ceabb22
Jeremy Echols <jechols@uoregon.edu>
2022-09-20 10:55:30 -0700

    Add custom JS to hide most 856 links

---
+++ custom/01ALLIANCE_UO-UO/js/80-hide-unnecessary-links.js
@@ -0,0 +1,8 @@
+app.value('linksToKeep', [
+  'Connect to the online finding aid for this collection',
+  'Online access to digitized portions of this collection',
+  'Notice of Interest in Unprocessed Collections',
+  'Online access to digitized portions of this item',
+  'Table of contents',
+  'Display Source Record',
+]);
