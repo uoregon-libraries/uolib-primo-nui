cf6628ff3fb41786ea58a5798f45db1a2e5862de
Jeremy Echols <jechols@uoregon.edu>
2022-09-21 10:11:47 -0700

    Add periods at the end of links we want to keep
    Inconsistent data entry or something - this gets us moving for now at
    least.

--- custom/01ALLIANCE_UO-UO/js/80-hide-unnecessary-links.js
+++ custom/01ALLIANCE_UO-UO/js/80-hide-unnecessary-links.js
@@ -1,8 +1,14 @@
 app.value('linksToKeep', [
+  'Connect to the online finding aid for this collection.',
   'Connect to the online finding aid for this collection',
+  'Online access to digitized portions of this collection.',
   'Online access to digitized portions of this collection',
   'Notice of Interest in Unprocessed Collections',
+  'Notice of Interest in Unprocessed Collections.',
   'Online access to digitized portions of this item',
+  'Online access to digitized portions of this item.',
   'Table of contents',
+  'Table of contents.',
   'Display Source Record',
+  'Display Source Record.',
 ]);
